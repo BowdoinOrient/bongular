62007fa393800037ec6fafe39d4d3289226499fe
Brian Jacobel <bjacobel@gmail.com>
2014-07-16 21:18:59 -0400

    To the person who popularized two tabs instead of four: I will find you, and I will kill you

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,12 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name bongularApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the bongularApp
- */
-angular.module('bongularApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+* @ngdoc function
+* @name bongularApp.controller:MainCtrl
+* @description
+* # MainCtrl
+* Controller of the bongularApp
+*/
+angular.module('bongularApp').controller('MainCtrl', function ($scope) {
+
+});
