4cdc686a1af70950ad2f2639b433b4d180e4b1ad
Brian Jacobel <bjacobel@gmail.com>
2014-08-03 21:23:05 -0400

    Mini header swings in on scroll & large viewports

--- app/scripts/controllers/core/core_directives.js
+++ app/scripts/controllers/core/core_directives.js
@@ -1,13 +1,32 @@
 angular.module('Core.directives', [])
 .directive("scrollmin", function ($window) {
     return function(scope, element, attrs) {
-        angular.element($window).bind("scroll", function() {
-            if (this.pageYOffset >= 150) {
-                scope.minifyHeader = true;
+        checkWidth = function(){
+            if (this.innerWidth <= 635) {
+                scope.minByWidth = true;
             } else {
-                scope.minifyHeader = false;
+                scope.minByWidth = false;
+            }
+        };
+
+        checkWidth();
+
+        angular.element($window).bind("resize", function(){
+            checkWidth();
+            scope.$apply();
+        });
+
+        angular.element($window).bind("scroll", function() {
+            if (this.pageYOffset >= 145) {
+                scope.minByScroll = true;
+                scope.flipIn = true;
+            } else {
+                scope.minByScroll = false;
+                scope.flipIn = false;
             }
             scope.$apply();
         });
+
+
     };
 });
